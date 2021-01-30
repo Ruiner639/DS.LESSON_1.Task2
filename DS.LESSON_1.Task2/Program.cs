4a770b5610d3a42a3da4e817acbf2e60a1c0b8fd
Dmitriy <70809857+Ruiner639@users.noreply.github.com>
2021-01-30 13:12:35 +0200

    Добавьте файлы проекта.

---
+++ DS.LESSON_1.Task2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace DS.LESSON_1.Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = "Write a C# Sharp Program to display the following pattern using the alphabet";
+            string[] list = str.Split(" ");
+            int max = 0, a = 0, result = 0;
+            while (a < list.Length)
+            {
+                if (list[a].Length > max)
+                {
+                    max = list[a].Length;
+                    result = a;
+                }
+                a++;
+            }
+            Console.WriteLine(list[result]);
+        }
+    }
+}
